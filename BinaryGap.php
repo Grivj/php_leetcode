82419e2ebf8f8411339971f57564b0fae05aaf0d
Jordan Griveau <jgriveau@yahoo.fr>
2022-02-04 19:46:21 +0100

    Create BinaryGap.php

---
+++ BinaryGap.php
@@ -0,0 +1,35 @@
+<?php
+
+// function solution($N) {
+//     $binary = decbin($N);
+//     echo "$binary <br/>";
+//     $curGap = 0;
+//     $maxGap = 0;
+//     foreach (str_split($binary) as $b) {
+//         if ($b == 1) { // closing or opening a gap
+//             $maxGap = max($maxGap, $curGap);
+//             $curGap = 0;
+//         } else {
+//             $curGap++;
+//         }
+//         echo "$b, $curGap, $maxGap <br/>";
+
+//     }
+//     return $maxGap;
+// }
+
+function solution($N)
+{
+    $binary = decbin($N);
+    $zeros = explode('1', $binary);
+    // We only count zeros that are inside 1s.
+    // Therefor we pop the last and then first exploded elements.
+    unset($zeros[count($zeros) - 1]);
+    unset($zeros[0]);
+    return max(
+        array_map('strlen', $zeros)
+    );
+
+}
+
+echo solution(100);
