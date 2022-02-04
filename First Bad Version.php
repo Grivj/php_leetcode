8b48001a038f22fe1eb645959b6bc86b87f2cb4b
Jordan Griveau <jgriveau@yahoo.fr>
2022-02-04 12:32:29 +0100

    Update First Bad Version.php

--- First Bad Version.php
+++ First Bad Version.php
@@ -1,5 +1,30 @@
 <?php
-function firstBadVersion($n)
-{
+/* The isBadVersion API is defined in the parent class VersionControl.
+public function isBadVersion($version){} */
 
+class Solution extends VersionControl
+{
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    public function firstBadVersion($n)
+    {
+        $first = 0;
+        $last = $n;
+        if ($this->isBadVersion($first)) {
+            return $first;
+        }
+        while ($first < $last) {
+            $middle = floor(($first + $last) / 2);
+
+            if ($this->isBadVersion($middle)) {
+                $last = $middle;
+            } else {
+                $first = $middle + 1;
+            }
+        }
+        return $first;
+
+    }
 }
